855fb2007677fa709801c80ef487ee5dee626563
Stephen Handley <stephen.handley@gmail.com>
2012-05-06 02:22:15 -0700

    duh

---
+++ README.md
@@ -0,0 +1,37 @@
+# Description
+
+Test grouping and formatting to make working with vanilla node assert marginally easier
+
+# Latest Version
+
+0.1.0
+
+# Installation
+```
+npm install tressa
+```
+
+or in package.json
+
+```json
+{
+  ...
+  "dependencies": {
+    "tressa": "~0.1.0"
+  }
+}
+```
+
+# Usage
+```js
+assert = require('assert')
+tressa = require('tressa')
+
+tressa({
+  "<Name for group>": function() {
+    assert.equal("<expected>", "<actual>", "<message>");
+    assert.equal("<expected2>", "<actual2>", "<message2>");
+    // ...
+  }
+})
+```
---
+++ index.js
@@ -0,0 +1,34 @@
+// adapted from https://github.com/Marak/colors.js/blob/master/colors.js
+var stylize = function(str, style_name) {
+  var style = {
+    'bold'      : ['\033[1m',  '\033[22m'],
+    'underline' : ['\033[4m',  '\033[24m'],
+    'green'     : ['\033[32m', '\033[39m'],
+    'red'       : ['\033[31m', '\033[39m'],
+  }[style_name];
+  return style[0] + str + style[1];
+};
+
+var tressa = function(groups) {
+  for (label in groups) {
+    if (groups.hasOwnProperty(label)) {
+      var group = groups[label];
+      label = stylize(label, 'bold');
+      
+      console.log('');
+      try {
+        group();
+        console.log(stylize(':) ' + label, 'green'));
+      } catch (error) {
+        console.log(stylize(':( ' + label, 'red'));
+        var diff = 'Expected ' + stylize(error.expected, 'bold') + ' but got ' + stylize(error.actual, 'bold');
+        console.log('   ' + stylize(error.message, 'underline') + ': ' + diff);
+      }
+    }
+  }
+  console.log("")
+}
+
+module.exports = tressa;
+
+
