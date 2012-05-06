e06b25cc966f04423fbfeda7f1f662787e3ec6b6
Stephen Handley <stephen.handley@gmail.com>
2012-05-06 02:37:30 -0700

    close

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Test grouping and formatting to make working with vanilla node assert marginally
 
 # Latest Version
 
-0.1.0
+0.1.1
 
 # Installation
 ```
@@ -17,7 +17,7 @@ or in package.json
 {
   ...
   "dependencies": {
-    "tressa": "~0.1.0"
+    "tressa": "~0.1.1"
   }
 }
 ```
--- index.js
+++ index.js
@@ -1,34 +1,35 @@
-// adapted from https://github.com/Marak/colors.js/blob/master/colors.js
-var stylize = function(str, style_name) {
-  var style = {
-    'bold'      : ['\033[1m',  '\033[22m'],
-    'underline' : ['\033[4m',  '\033[24m'],
-    'green'     : ['\033[32m', '\033[39m'],
-    'red'       : ['\033[31m', '\033[39m'],
-  }[style_name];
-  return style[0] + str + style[1];
-};
+(function() {
+  // adapted from https://github.com/Marak/colors.js/blob/master/colors.js
+  var stylize = function(str, style_name) {
+    var style = {
+      'bold'      : ['\033[1m',  '\033[22m'],
+      'underline' : ['\033[4m',  '\033[24m'],
+      'green'     : ['\033[32m', '\033[39m'],
+      'red'       : ['\033[31m', '\033[39m'],
+    }[style_name];
+    return style[0] + str + style[1];
+  };
 
-var tressa = function(groups) {
-  for (label in groups) {
-    if (groups.hasOwnProperty(label)) {
-      var group = groups[label];
-      label = stylize(label, 'bold');
+  var tressa = function(groups) {
+    for (label in groups) {
+      if (groups.hasOwnProperty(label)) {
+        var group = groups[label];
+        label = stylize(label, 'bold');
       
-      console.log('');
-      try {
-        group();
-        console.log(stylize(':) ' + label, 'green'));
-      } catch (error) {
-        console.log(stylize(':( ' + label, 'red'));
-        var diff = 'Expected ' + stylize(error.expected, 'bold') + ' but got ' + stylize(error.actual, 'bold');
-        console.log('   ' + stylize(error.message, 'underline') + ': ' + diff);
+        console.log('');
+        try {
+          group();
+          console.log(stylize(':) ' + label, 'green'));
+        } catch (error) {
+          console.log(stylize(':( ' + label, 'red'));
+          var diff = 'Expected ' + stylize(error.expected, 'bold') + ' but got ' + stylize(error.actual, 'bold');
+          console.log('   ' + stylize(error.message, 'underline') + ': ' + diff);
+        }
       }
     }
+    console.log("");
   }
-  console.log("");
-}
 
-module.exports = tressa;
-
+  module.exports = tressa;
+})();
 
